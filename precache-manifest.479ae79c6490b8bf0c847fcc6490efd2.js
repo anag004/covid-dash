68d2a7f08aa4588e33c6f2284f8b235de2fd259b
Ananye Agarwal <ananayagarwal@gmail.com>
2020-06-08 18:03:01 +0530

    Updates

---
+++ precache-manifest.479ae79c6490b8bf0c847fcc6490efd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcf836a167b7d0b9934202cdce0605b",
+    "url": "/covid-dash/index.html"
+  },
+  {
+    "revision": "c0185db9a6ce2e4875b8",
+    "url": "/covid-dash/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45577962fd02ca514664",
+    "url": "/covid-dash/static/js/2.4f304f89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-dash/static/js/2.4f304f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0185db9a6ce2e4875b8",
+    "url": "/covid-dash/static/js/main.ecedd6d9.chunk.js"
+  },
+  {
+    "revision": "86493b6978f1c8a634d1",
+    "url": "/covid-dash/static/js/runtime-main.277f1d42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-dash/static/media/logo.5d5d9eef.svg"
+  }
+]);
