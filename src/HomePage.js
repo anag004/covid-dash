423c0638040189062341c84561a61408904fe27c
Ananye Agarwal <ananayagarwal@gmail.com>
2020-06-09 00:44:05 +0530

    Added NYC graph

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Menu, Typography, Divider } from 'antd';
+import NYC from "./graphs/NYC";
 const { Content } = Layout;
 const { Title, Paragraph } = Typography;
 
@@ -11,6 +12,7 @@ function HomePage() {
             You can see below various interactive graphs for regions in India and around the world. The red region in each plot indicates that the model is extrapolating deaths in that region. You can use the scrubber to change the amount of data the model fits on and see how that affects the uncertainty intervals. 
         </Paragraph>
         <Title level={2}>New York City</Title>
+        <NYC/>
       </Content>
     )
 }
---
+++ src/graphs/NYC.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Runtime, Inspector } from "@observablehq/runtime";
+import notebook from "6505d7f8e11d1d3a";
+import { Slider } from "antd";
+
+class NYC extends Component {
+    chartRef = React.createRef();
+    state = { bpoint: 60 };
+
+    setBpoint = (event) => {
+        console.log(event);
+        this.setState({ bpoint: event });
+    }
+
+    componentDidMount() {
+        const runtime = new Runtime();
+        runtime.module(notebook, name => {
+            if (name === "chart") {
+                return new Inspector(this.chartRef.current);
+            }
+
+            if (name === "mutable bpoint") {
+                return {
+                    fulfilled: (value) => {
+                        this.bpoint = value;
+                    }
+                };
+            }
+        });
+    }
+
+    componentDidUpdate(nextProps, nextState) {
+        if (nextState.bpoint != this.state.bpoint) {
+            this.bpoint.value = nextState.bpoint;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div ref={this.chartRef}></div>
+                <large>Scrub to set projection area</large>
+                <br />
+                {/* <input type="range" min="60" max="137" step="1" */}
+                {/* value={this.state.bpoint} onChange={this.setBpoint} /> */}
+                <Slider min={60} max={137} step={1} tooltipVisible={true} onChange={this.setBpoint} value={this.state.bpoint} tooltipPlacement="bottom"></Slider>
+            </div>
+        )
+    }
+}
+
+export default NYC;
